cec4789b9a672be9aa58a91f6adc643e59bcaa7e
liuliang960206 <2395367800@qq.com>
2019-04-23 09:33:21 +0800

    cases

--- case/多图片切换/think.js
+++ case/多图片切换/think.js
@@ -11,10 +11,9 @@ const span2 = document.getElementById('span2');
 const pre = document.getElementById('pre');
 const next = document.getElementById('next');
 
-
+//左右两侧
 function banner(after, before, imgs, p, span, a, b) {
-    
-    function fn(index1,n,m) {
+    function fn(index1, n, m) {
         imgs[index].style.display = 'none';
         index = index1;
         index == n ? index = m : null;
@@ -24,62 +23,61 @@ function banner(after, before, imgs, p, span, a, b) {
     };
     let index = 0; //当前图片的序号 当点击左侧按钮时，让序号-1；当点击右侧按钮时候，让序号+1  (为什么index=0可以，index=1不可以？)
     after.onclick = function () {
-        fn(index+1,a,0);
+        fn(index + 1, a, 0);
     };
     before.onclick = function () {
-        fn(index-1,-1,b);
+        fn(index - 1, -1, b);
     };
 };
 banner(after1, before1, imgs, p1, span1, 4, 3);
 banner(after2, before2, imgs2, p2, span2, 3, 2);
 
-
-
-// 当点击上一组，左侧图片序号-1，右侧图片序号-1 
+// 上一组下一组封装
 let index = 0;
 let index2 = 0;
-pre.onclick = function () {
+function left1(imgs, p, span, a, b) {
     imgs[index].style.display = 'none';
     index--;
-    index == -1 ? index = 3 : null;
-    p1.innerHTML = `这是第${index + 1}张图片`;
-    span1.innerHTML = `${(index + 1)}/4`;
+    index == -1 ? index = a : null;
+    p.innerHTML = `这是第${index + 1}张图片`;
+    span.innerHTML = `${(index + 1)}/${b}`;
     imgs[index].style.display = 'block';
+};
 
-    imgs2[index2].style.display = 'none';
+function left2(imgs, p, span, a, b) {
+    imgs[index2].style.display = 'none';
     index2--;
-    index2 == -1 ? index2 = 2 : null;
-    p2.innerHTML = `这是第${index2 + 1}张图片`;
-    span2.innerHTML = `${(index2 + 1)}/3`;
-    imgs2[index2].style.display = 'block';
+    index2 == -1 ? index2 = a : null;
+    p.innerHTML = `这是第${index2 + 1}张图片`;
+    span.innerHTML = `${(index2 + 1)}/${b}`;
+    imgs[index2].style.display = 'block';
+};
+
+function right1(imgs, p, span, a, b) {
+    imgs[index].style.display = 'none';
+    index++;
+    index == a ? index = 0 : null;
+    p1.innerHTML = `这是第${index + 1}张图片`;
+    span1.innerHTML = `${(index + 1)}/${a}`;
+    imgs[index].style.display = 'block';
+};
+
+function right2(imgs, p, span, a, b){
+    imgs[index2].style.display = 'none';
+    index2++;
+    index2 == a ? index2 = 0 : null;
+    p.innerHTML = `这是第${index2 + 1}张图片`;
+    span.innerTML = `${(index2 + 1)}/${a}`;
+    imgs[index2].style.display = 'block';
+};
+
+pre.onclick = function () {
+    left1(imgs, p1, span1, 3, 4);
+    left2(imgs2, p2, span2, 2, 3);
 };
 
 next.onclick = function () {
-    imgs[index].style.display = 'none';
-    index++;
-    index == 4 ? index = 0 : null;
-    p1.innerHTML = `这是第${index + 1}张图片`;
-    span1.innerHTML = `${(index + 1)}/4`;
-    imgs[index].style.display = 'block';
-
-    imgs2[index2].style.display = 'none';
-    index2++;
-    index2 == 3 ? index2 = 0 : null;
-    p2.innerHTML = `这是第${index2 + 1}张图片`;
-    span2.innerHTML = `${(index2 + 1)}/3`;
-    imgs2[index2].style.display = 'block';
+    right1(imgs, p1, span1, 4);
+    right2(imgs2, p2, span2, 3);
 };
 
-// // 上一组下一组封装？？？
-// function fn(imgs, index, a, p, span, b) {
-//     imgs[index].style.display = 'none';
-//     index--;
-//     index == -1 ? index = a : null;
-//     p.innerHTML = `这是第${index + 1}张图片`;
-//     span.innerHTML = `${(index + 1)}/${b}`;
-//     imgs[index].style.display = 'block';
-// };
-// pre.onclick = function () {
-//     fn(imgs, index, 3, p1, span1, 4);
-//     // fn(imgs2, index2, 2, p2, span2, 3);
-// };
