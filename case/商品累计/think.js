992f8274db6c2944da1ae5caa391aac37f820d31
liuliang960206 <2395367800@qq.com>
2019-04-29 18:43:26 +0800

    cases

--- case/商品累计/think.js
+++ case/商品累计/think.js
@@ -1,63 +1,97 @@
-
-const subtract = document.querySelectorAll('.list>li>i:nth-of-type(1)');
-const add = document.querySelectorAll('.list>li>i:nth-of-type(2)');
-const em = document.querySelectorAll('.list>li>em');
-const price = document.querySelectorAll('.list>li>span>strong:nth-of-type(1)');
-const subtotal = document.querySelectorAll('.list>li>span>strong:nth-of-type(2)');
-
-//总量 总价 最高价
-const total = document.querySelectorAll('.info>span:nth-of-type(1)>em');
-const totalPrice = document.querySelectorAll('.info>span:nth-of-type(2)>em');
-const highestPrice = document.querySelectorAll('.info>span:nth-of-type(3)>em');
-
-//数量 小计
-let ary = [];
-for (let i = 0; i < add.length; i++) {
-    let num = 0;
-    let priceNum = parseFloat(price[i].innerHTML);
-    ary.push(priceNum);
-    add[i].onclick = function () {
-        num++;
-        em[i].innerHTML = num;
-        subtotal[i].innerHTML = priceNum * num + '元';
-        fn();
-        fn1();
+let ary = [
+    {
+        price: 12.5
+    },
+    {
+        price: 10.5
+    },
+    {
+        price: 8.5
+    },
+    {
+        price: 8
+    },
+    {
+        price: 14.5
+    },
+];
+class Shop {
+    constructor(ary) {
+        this.ul = document.querySelector('ul');
+        this.i1s = this.ul.querySelectorAll('li>i:nth-of-type(1)');
+        this.i2s = this.ul.querySelectorAll('li>i:nth-of-type(2)');
+        this.ems = this.ul.querySelectorAll('li>em');
+        this.strong1 = this.ul.querySelectorAll('li>span>strong:nth-of-type(1)');
+        this.strong2 = this.ul.querySelectorAll('li>span>strong:nth-of-type(2)');
+        this.em1 = document.querySelectorAll('.info>span:nth-of-type(1)>em')[0]
+        this.em2 = document.querySelectorAll('.info>span:nth-of-type(2)>em')[0]
+        this.em3 = document.querySelectorAll('.info>span:nth-of-type(3)>em')[0];
+        this.num = [0, 0, 0, 0, 0];
+        this.price = ary;
+        // this.count = 0;
     };
-    subtract[i].onclick = function () {
-        if (num === 0) return;
-        num--;
-        em[i].innerHTML = num;
-        subtotal[i].innerHTML = priceNum * num + '元';
-        fn();
-        fn1();
+    add() {
+        for (let i = 0; i < this.i2s.length; i++) {
+            this.i2s[i].onclick = () => {
+                this.num[i]++;
+                this.ems[i].innerHTML = this.num[i];
+                this.subtotal();
+                this.total();
+                this.totalPrice();
+                this.highestPrice();
+            }
+        };
+    };
+    subtract() {
+        for (let i = 0; i < this.i1s.length; i++) {
+            this.i1s[i].onclick = () => {
+                if (this.num[i] === 0) return;
+                this.num[i]--;
+                this.ems[i].innerHTML = this.num[i];
+                this.subtotal();
+                this.total();
+                this.totalPrice();
+                this.highestPrice();
+            }
+        }
+    };
+    subtotal() {
+        for (let i = 0; i < this.strong2.length; i++) {
+            this.strong2[i].innerHTML = this.price[i].price * this.num[i] + '元';
+        };
+    };
+    total() {
+        let count = 0;
+        for (let i = 0; i < this.ems.length; i++) {
+            count += parseFloat(this.ems[i].innerHTML);
+        };
+        this.em1.innerHTML = count;
+    };
+    totalPrice() {
+        let count = 0;
+        for (let i = 0; i < this.ems.length; i++) {
+            count += parseFloat(this.strong2[i].innerHTML);
+        };
+        this.em2.innerHTML = count;
+    };
+    highestPrice() {
+        let max = 0;
+        for (let i = 0; i < this.ems.length; i++) {
+            if(this.ems[i] !== '0'){
+                let price = parseFloat(this.strong2.innerHTML);
+                if(max<price){
+                    max = price;
+                    this.em3.innerHTML = max;
+                    console.log(this.em3)
+                };
+            };
+        };
     };
 };
-
-// 最高单价
-ary.sort(function(a,b){
-    return a-b;
-});
-let high = ary.splice(ary.length-1,1)[0];
-highestPrice[0].innerHTML = high;
-
-// 总量
-function fn() {
-    let count = 0;
-    for (let i = 0; i < em.length; i++) {
-        count += parseFloat(em[i].innerHTML);
-    };
-    total[0].innerHTML = count;
-};
-//总价
-function fn1() {
-    let count = 0;
-    for (let i = 0; i < em.length; i++) {
-        count += parseFloat(subtotal[i].innerHTML);
-    };
-    totalPrice[0].innerHTML = count;
-};
-
-
-
-
-
+let c = new Shop(ary);
+c.add();
+c.subtract();
+c.subtotal();
+c.total();
+c.totalPrice();
+c.highestPrice();
---
+++ case/多图片切换/think1.js
@@ -0,0 +1,83 @@
+const before1 = document.getElementById('before1');
+const before2 = document.getElementById('before2');
+const after1 = document.getElementById('after1');
+const after2 = document.getElementById('after2');
+const imgs = document.querySelectorAll('#oDiv1>img')
+const imgs2 = document.querySelectorAll('#oDiv2>img')
+const p1 = document.getElementById('p1');
+const p2 = document.getElementById('p2');
+const span1 = document.getElementById('span1');
+const span2 = document.getElementById('span2');
+const pre = document.getElementById('pre');
+const next = document.getElementById('next');
+
+//左右两侧s
+function banner(after, before, imgs, p, span, a, b) {
+    function fn(index1, n, m) {
+        imgs[index].style.display = 'none';
+        index = index1;
+        index == n ? index = m : null;
+        p.innerHTML = `这是第${index + 1}张图片`;
+        span.innerHTML = `${(index + 1)}/${a}`;
+        imgs[index].style.display = 'block';
+    };
+    let index = 0; //当前图片的序号 当点击左侧按钮时，让序号-1；当点击右侧按钮时候，让序号+1  (为什么index=0可以，index=1不可以？)
+    after.onclick = function () {
+        fn(index + 1, a, 0);
+    };
+    before.onclick = function () {
+        fn(index - 1, -1, b);
+    };
+};
+banner(after1, before1, imgs, p1, span1, 4, 3);
+banner(after2, before2, imgs2, p2, span2, 3, 2);
+
+// 上一组下一组封装
+let index = 0;
+let index2 = 0;
+function left1(imgs, p, span, a, b) {
+    imgs[index].style.display = 'none';
+    index--;
+    index == -1 ? index = a : null;
+    p.innerHTML = `这是第${index + 1}张图片`;
+    span.innerHTML = `${(index + 1)}/${b}`;
+    imgs[index].style.display = 'block';
+};
+
+function left2(imgs, p, span, a, b) {
+    imgs[index2].style.display = 'none';
+    index2--;
+    index2 == -1 ? index2 = a : null;
+    p.innerHTML = `这是第${index2 + 1}张图片`;
+    span.innerHTML = `${(index2 + 1)}/${b}`;
+    imgs[index2].style.display = 'block';
+};
+
+function right1(imgs, p, span, a, b) {
+    imgs[index].style.display = 'none';
+    index++;
+    index == a ? index = 0 : null;
+    p1.innerHTML = `这是第${index + 1}张图片`;
+    span1.innerHTML = `${(index + 1)}/${a}`;
+    imgs[index].style.display = 'block';
+};
+
+function right2(imgs, p, span, a, b){
+    imgs[index2].style.display = 'none';
+    index2++;
+    index2 == a ? index2 = 0 : null;
+    p.innerHTML = `这是第${index2 + 1}张图片`;
+    span.innerTML = `${(index2 + 1)}/${a}`;
+    imgs[index2].style.display = 'block';
+};
+
+pre.onclick = function () {
+    left1(imgs, p1, span1, 3, 4);
+    left2(imgs2, p2, span2, 2, 3);
+};
+
+next.onclick = function () {
+    right1(imgs, p1, span1, 4);
+    right2(imgs2, p2, span2, 3);
+};
+
